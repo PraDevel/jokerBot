d977f09375f3c2a22c1aef2ed2de34ce02ddff2f
PRATYUSH-ROY <66131007+PRATYUSH-ROY@users.noreply.github.com>
2021-05-09 13:54:16 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import datetime
+import math
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import requests
+import json
+STATE = None
+BIRTH_YEAR = 1
+BIRTH_MONTH = 2
+BIRTH_DAY = 3
+
+# function to handle the /start command
+def start(update, context):
+    update.message.reply_text(f"Hi , nice to meet you!")
+
+
+def joke(update, context):
+    r = requests.get("https://official-joke-api.appspot.com/random_joke")
+    ppp=(r.json())
+    update.message.reply_text(str(ppp['setup']+"  "+ str(ppp['punchline'])))
+
+def main():
+    TOKEN = "1789169829:AAHh5KSoeUsiZx8e9iVLq9xICLFV1XhjOig"
+    updater = Updater(TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("joke", joke))
+
+
+
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
